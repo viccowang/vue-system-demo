095ffbed1ec1fce61de24d26929763dd6aaea530
viccowang <viccowang@gmail.com>
2018-02-11 14:38:01 +0800

    update base construction files

--- source/admin/src/store/getters.js
+++ source/admin/src/store/getters.js
@@ -3,7 +3,8 @@ const getters = {
   userName: state => state.user.name,
   avatar: state => state.user.avatar,
   token: state => state.user.token,
-  roles: state => state.user.roles
+  roles: state => state.user.roles,
+  asyncRouter: state => state.asyncRouter.router
 }
 
 export default getters
--- source/admin/src/store/index.js
+++ source/admin/src/store/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 // custom store
 import user from './modules/user'
+import asyncRouter from './modules/asyncRouter'
 import getters from './getters'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    user
+    user,
+    asyncRouter
   },
   getters
 })
