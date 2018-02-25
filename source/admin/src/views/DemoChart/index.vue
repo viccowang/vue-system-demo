69a645eeee6fdf289eddc4db97c64cf3e6fd1e74
Vicco Wang <viccowang@gmail.com>
2018-02-25 17:04:10 +0800

    引入了jsx语法插件和class语法插件.丰富编程体验.怎么写都可以~~

--- source/admin/src/views/DemoChart/index.vue
+++ source/admin/src/views/DemoChart/index.vue
@@ -2,144 +2,28 @@
     <div>
        <h4>Demo Chart</h4>
        进行了页面缓存,noCache值为false <p/>
-      <input type='text'>
+       该页面为动态增加自定义组件Demo <p/>
+      <button @click="addChart('e-chart','barChar1Data')">Add Chart1</button>
+      <button @click="addChart('e-chart','barChar2Data')">Add Chart2</button>
       <br>
-      <e-chart :options="barChartOpts" />
-      <br>
-      <e-chart :options="bar2chartOpts" />
+      <echart-coms :charts="charts"/>
     </div>
 </template>
 
 <script>
-import EChart from 'vue-echarts/components/Echarts'
-
-import 'echarts/lib/chart/bar'
-import 'echarts/lib/chart/line'
-import 'echarts/lib/component/toolbox'
-import 'echarts/lib/component/tooltip'
+import echartComs from './echartComs'
 
 export default {
   name: 'demoChart',
-  components: { EChart },
-  data: () => {
+  components: { echartComs },
+  data () {
     return {
-      barChartOpts: {
-        xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [
-          {
-            data: [120, 200, 150, 80, 70, 110, 130],
-            type: 'bar'
-          }
-        ]
-      },
-      bar2chartOpts: {
-        title: {
-          text: '某地区蒸发量和降水量',
-          subtext: '纯属虚构'
-        },
-        tooltip: {
-          trigger: 'axis'
-        },
-        legend: {
-          data: ['蒸发量', '降水量']
-        },
-        toolbox: {
-          show: true,
-          feature: {
-            dataView: { show: true, readOnly: false },
-            magicType: { show: true, type: ['line', 'bar'] },
-            restore: { show: true },
-            saveAsImage: { show: true }
-          }
-        },
-        calculable: true,
-        xAxis: [
-          {
-            type: 'category',
-            data: [
-              '1月',
-              '2月',
-              '3月',
-              '4月',
-              '5月',
-              '6月',
-              '7月',
-              '8月',
-              '9月',
-              '10月',
-              '11月',
-              '12月'
-            ]
-          }
-        ],
-        yAxis: [
-          {
-            type: 'value'
-          }
-        ],
-        series: [
-          {
-            name: '蒸发量',
-            type: 'bar',
-            data: [
-              2.0,
-              4.9,
-              7.0,
-              23.2,
-              25.6,
-              76.7,
-              135.6,
-              162.2,
-              32.6,
-              20.0,
-              6.4,
-              3.3
-            ],
-            markPoint: {
-              data: [
-                { type: 'max', name: '最大值' },
-                { type: 'min', name: '最小值' }
-              ]
-            },
-            markLine: {
-              data: [{ type: 'average', name: '平均值' }]
-            }
-          },
-          {
-            name: '降水量',
-            type: 'bar',
-            data: [
-              2.6,
-              5.9,
-              9.0,
-              26.4,
-              28.7,
-              70.7,
-              175.6,
-              182.2,
-              48.7,
-              18.8,
-              6.0,
-              2.3
-            ],
-            markPoint: {
-              data: [
-                { name: '年最高', value: 182.2, xAxis: 7, yAxis: 183 },
-                { name: '年最低', value: 2.3, xAxis: 11, yAxis: 3 }
-              ]
-            },
-            markLine: {
-              data: [{ type: 'average', name: '平均值' }]
-            }
-          }
-        ]
-      }
+      charts: []
+    }
+  },
+  methods: {
+    addChart (componentName, data) {
+      this.charts.push({name: componentName, data: data})
     }
   }
 }
